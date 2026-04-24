af43b9f3e73b57c15067a87cbdbcef3d295e7a5d
fgf <you@example.com>
2026-04-24 16:03:25 +0530

    updated entity

---
+++ src/main/java/com/cg/entity/Review.java
@@ -0,0 +1,65 @@
+package com.cg.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer reviewId;
+
+    private Integer reservationId;
+
+    private Integer rating;
+
+    private String comment;
+    
+    private LocalDate reviewDate;
+
+    public Integer getReviewId() {
+		return reviewId;
+	}
+
+	public void setReviewId(Integer reviewId) {
+		this.reviewId = reviewId;
+	}
+
+	public Integer getReservationId() {
+		return reservationId;
+	}
+
+	public void setReservationId(Integer reservationId) {
+		this.reservationId = reservationId;
+	}
+
+	public Integer getRating() {
+		return rating;
+	}
+
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public LocalDate getReviewDate() {
+		return reviewDate;
+	}
+
+	public void setReviewDate(LocalDate reviewDate) {
+		this.reviewDate = reviewDate;
+	}
+
+	
+
+    
+}
