b43673e34f084e9f81708eb2d67634e7c7176506
dar2005 <darshikagrover2005@gmail.com>
2026-04-24 15:34:56 +0530

    first commit

---
+++ src/main/java/com/cg/entity/Hotel.java
@@ -0,0 +1,83 @@
+package com.cg.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "hotel")
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hotel_id")
+    private Long hotelId;
+
+    @Column(nullable = false)
+    private String name;
+    private String location;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+ 
+    public Hotel() {
+		super();
+	}
+
+	public Hotel(Long hotelId, String name, String location, String description, List<Amenity> amenities) {
+		super();
+		this.hotelId = hotelId;
+		this.name = name;
+		this.location = location;
+		this.description = description;
+		this.amenities = amenities;
+	}
+
+	@ManyToMany
+    @JoinTable(
+            name = "hotel_amenity",
+            joinColumns = @JoinColumn(name = "hotel_id"),
+            inverseJoinColumns = @JoinColumn(name = "amenity_id")
+    )
+    private List<Amenity> amenities;
+
+	public Long getHotelId() {
+		return hotelId;
+	}
+
+	public void setHotelId(Long hotelId) {
+		this.hotelId = hotelId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Amenity> getAmenities() {
+		return amenities;
+	}
+
+	public void setAmenities(List<Amenity> amenities) {
+		this.amenities = amenities;
+	}
+	
+}
---
+++ src/main/java/com/cg/entity/Room.java
@@ -0,0 +1,107 @@
+package com.cg.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "room")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roomId;
+
+    private int roomNumber;
+    private boolean isAvailable;
+
+    @ManyToOne
+    @JoinColumn(name = "room_type_id")
+    private RoomType roomType;
+    
+    
+
+    public Room() {
+		super();
+	}
+
+	public Room(Long roomId, int roomNumber, boolean isAvailable, RoomType roomType, List<Amenity> amenities,
+			List<Reservation> reservations) {
+		super();
+		this.roomId = roomId;
+		this.roomNumber = roomNumber;
+		this.isAvailable = isAvailable;
+		this.roomType = roomType;
+		this.amenities = amenities;
+		this.reservations = reservations;
+	}
+
+	@ManyToMany
+    @JoinTable(
+        name = "room_amenity",
+        joinColumns = @JoinColumn(name = "room_id"),
+        inverseJoinColumns = @JoinColumn(name = "amenity_id")
+    )
+    private List<Amenity> amenities;
+
+    @OneToMany(mappedBy = "room")
+    private List<Reservation> reservations;
+
+
+
+	public Long getRoomId() {
+		return roomId;
+	}
+
+	public void setRoomId(Long roomId) {
+		this.roomId = roomId;
+	}
+
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+
+	public RoomType getRoomType() {
+		return roomType;
+	}
+
+	public void setRoomType(RoomType roomType) {
+		this.roomType = roomType;
+	}
+
+	public List<Amenity> getAmenities() {
+		return amenities;
+	}
+
+	public void setAmenities(List<Amenity> amenities) {
+		this.amenities = amenities;
+	}
+
+	public List<Reservation> getReservations() {
+		return reservations;
+	}
+
+	public void setReservations(List<Reservation> reservations) {
+		this.reservations = reservations;
+	}
+    
+}
