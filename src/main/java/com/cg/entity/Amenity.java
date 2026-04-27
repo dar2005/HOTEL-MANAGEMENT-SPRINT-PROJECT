b3d1c54e47e2653a3538b8556c16cbe9dcc6895a
Harshit9670 <tiwarih463@gmail.com>
2026-04-27 10:38:11 +0530

    Updated

--- src/main/java/com/cg/entity/Amenity.java
+++ src/main/java/com/cg/entity/Amenity.java
@@ -5,13 +5,34 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "amenity")
 public class Amenity {
-
-    @Id
+	
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer amenityId;
 
     private String name;
     private String description;
 
-    // Getters and Setters
+
+    public Integer getAmenityId() {
+		return amenityId;
+	}
+	public void setAmenityId(Integer amenityId) {
+		this.amenityId = amenityId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+
+
 }
