f5f5a80a185d29841575e27257db5778571f09c7
fgf <you@example.com>
2026-04-27 10:23:47 +0530

    Udated Review

--- src/main/java/com/cg/entity/Review.java
+++ src/main/java/com/cg/entity/Review.java
@@ -9,57 +9,68 @@ public class Review {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer reviewId;
+    @Column(name = "review_id")
+    private Long reviewId;
 
-    private Integer reservationId;
+    @Column(name = "rating")
+    private int rating;
 
-    private Integer rating;
-
+    @Column(name = "comment")
     private String comment;
-    
+
+    @Column(name = "review_date")
     private LocalDate reviewDate;
 
-    public Integer getReviewId() {
-		return reviewId;
-	}
-
-	public void setReviewId(Integer reviewId) {
-		this.reviewId = reviewId;
-	}
-
-	public Integer getReservationId() {
-		return reservationId;
-	}
-
-	public void setReservationId(Integer reservationId) {
-		this.reservationId = reservationId;
-	}
-
-	public Integer getRating() {
-		return rating;
-	}
-
-	public void setRating(Integer rating) {
-		this.rating = rating;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-
-	public LocalDate getReviewDate() {
-		return reviewDate;
-	}
-
-	public void setReviewDate(LocalDate reviewDate) {
-		this.reviewDate = reviewDate;
-	}
-
-	
+    
+    @ManyToOne
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
 
     
+    public Review() {}
+
+    public Review(int rating, String comment, LocalDate reviewDate, Reservation reservation) {
+        this.rating = rating;
+        this.comment = comment;
+        this.reviewDate = reviewDate;
+        this.reservation = reservation;
+    }
+
+    
+
+    public Long getReviewId() {
+        return reviewId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public LocalDate getReviewDate() {
+        return reviewDate;
+    }
+
+    public void setReviewDate(LocalDate reviewDate) {
+        this.reviewDate = reviewDate;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
 }
