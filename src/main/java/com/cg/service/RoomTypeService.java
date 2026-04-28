412201d8110b66eb3461077ab288f6312fa55d14
Adarsh Kesharwani <adarsh2301k@gmail.com>
2026-04-28 09:52:51 +0530

    created imple class

--- src/main/java/com/cg/service/RoomTypeService.java
+++ src/main/java/com/cg/service/RoomTypeService.java
@@ -1,45 +1,16 @@
 package com.cg.service;
 
-import com.cg.entity.RoomType;
-import com.cg.exception.ResourceNotFoundException;
-import com.cg.repo.RoomTypeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-public class RoomTypeService {
+import com.cg.entity.RoomType;
 
-    @Autowired
-    private RoomTypeRepository repo;
+public interface RoomTypeService {
 
-    // GET ALL
-    public List<RoomType> getAllTypes() {
-        return repo.findAll();
-    }
+    List<RoomType> getAllTypes();
 
-    // GET BY ID
-    public RoomType getById(Long id) {
-        return repo.findById(id)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("RoomType not found with id: " + id));
-    }
+    RoomType getById(Long id);
 
-    // GET BY NAME
-    public RoomType getByName(String name) {
-        return repo.findByTypeName(name)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("RoomType not found with name: " + name));
-    }
+    RoomType getByName(String name);
 
-    // CREATE
-    public RoomType create(RoomType rt) {
-
-        if (repo.findByTypeName(rt.getTypeName()).isPresent()) {
-            throw new RuntimeException("RoomType already exists: " + rt.getTypeName());
-        }
-
-        return repo.save(rt);
-    }
+    RoomType create(RoomType rt);
 }
