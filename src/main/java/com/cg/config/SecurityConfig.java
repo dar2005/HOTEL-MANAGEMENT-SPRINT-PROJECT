d341c1abdf9eee699aa4c7aaf994c2e9138443ea
Shubham Singh <as4390704@gmail.com>
2026-04-29 12:29:28 +0530

    tested swagger

--- src/main/java/com/cg/config/SecurityConfig.java
+++ src/main/java/com/cg/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class SecurityConfig {
@@ -37,10 +38,10 @@ public class SecurityConfig {
                         "/rooms/type/**",
                         "/hotels/**"
                 ).permitAll()
+                
+                .requestMatchers(HttpMethod.GET, "/reviews/**").permitAll()
 
-                // USER + ADMIN
-                .requestMatchers("/reviews/**")
-                .hasAnyRole("USER", "ADMIN")
+             
 
                 // USER only
                 .requestMatchers(
@@ -53,6 +54,11 @@ public class SecurityConfig {
                         "/api/amenities/**",
                         "/rooms/**"
                 ).hasRole("ADMIN")
+                
+                .requestMatchers(HttpMethod.POST, "/reviews/**").hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/reviews/**").hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/reviews/**").hasAnyRole("USER", "ADMIN")
+
 
                 .anyRequest().authenticated()
             )
